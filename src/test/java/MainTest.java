0e88eac988fe1900a3f9bc68b292d1eb13b3cab7
Eren <erenalptekin@sabanciuniv.edu>
2017-01-19 11:05:32 +0000

    a test that will fail added to check jenkins behaviour

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,13 @@
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class MainTest {
     @Test
     public void jenkinsAutoDeployPushes() {
-
+        assertThat(1, equalTo(2));
     }
 }
